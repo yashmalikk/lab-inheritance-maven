ef1c6a72e1f3a9d4a33a163ecf0b254057e87ee9
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-09-23 14:51:21 -0500

    Add our experiments class.

---
+++ src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
@@ -0,0 +1,23 @@
+package edu.grinnell.csc207.experiments;
+
+// import edu.grinnell.csc207.util.Counter;
+// import edu.grinnell.csc207.util.BasicCounter;
+
+import java.io.PrintWriter;
+
+/**
+ * Some experiments as we explore our various Counter classes.
+ */
+public class CounterExperiments {
+  /**
+   * Run our experiments.
+   *
+   * @param args
+   *   The command-line arguments. Ignored.
+   */
+  public static void main(String[] args) throws Exception {
+    PrintWriter pen = new PrintWriter(System.out, true);
+
+    pen.close();
+  } // main(String[])
+} // class CounterExperiments
